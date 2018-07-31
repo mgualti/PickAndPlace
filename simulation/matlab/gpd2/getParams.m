bedfec6d2734bcc4076283b3cf569373d9a8bf15
mgualti <mgualti@ccs.neu.edu>
2018-07-31 10:02:15 -0400

    gpd2 files were missing.

---
+++ simulation/matlab/gpd2/getParams.m
@@ -0,0 +1,20 @@
+% 
+% Set handparams and imageparams here
+% 
+function [handparams, imageparams] = getParams()
+    
+    % this corresponds to the actual hand (Robotiq 85)
+    handparams = handParameters();
+    handparams.fw = 0.01;
+    handparams.handOD = 0.105;
+    handparams.handDepth = 0.06;
+    handparams.handHeight = 0.02;
+    
+    % this corresponds to the hand used for training
+    imageparams = imageParameters();
+    imageparams.imageOD = 0.10;
+    imageparams.imageDepth = 0.06;
+    imageparams.imageHeight = 0.02;
+    imageparams.imageSize = 60;
+
+end
